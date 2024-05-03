66fc97aa0b992202296c21798602e8c81d5d62ef
c <fdsatrewq3@gmail.com>
2024-05-03 19:55:00 +0300

    Revert "Added lvl up debug command"
    This reverts commit 837c34945b68cef5c121e8f3ccdb8b1591fdb225.

--- game/scripts/vscripts/debug_tools.lua
+++ game/scripts/vscripts/debug_tools.lua
@@ -31,15 +31,6 @@ function Init()
         DebugDropTempleItem(lootQuality, isSoul, isArti)
     end, "droptempleitem", FCVAR_CHEAT)
 
-    Convars:RegisterCommand("templelvl", function(_, desiredLevel)
-        local desiredLevel = tonumber(lootQuality)
-        if(desiredLevel == nil) then
-            desiredLevel = 100
-        end
-
-        DebugTempleLevel(desiredLevel)
-    end, "templelvl", FCVAR_CHEAT)
-
     local villageDummyPoint = Vector(-14972.935547, 14804.335938, 128.000000)
     
     CreateUnitByName("npc_dota_creature_tutorial_dummy", villageDummyPoint, false, nil, nil, DOTA_TEAM_NEUTRALS)
@@ -57,25 +48,4 @@ function DebugDropTempleItem(lootQuality, isSoul, isArti)
     end
 
     COverthrowGameMode:DropTempleItem(PlayerResource:GetSelectedHeroEntity(0), 100, 2, lootQuality, isArti)
-end
-
-function DebugDropTempleItem(lootQuality, isSoul, isArti)
-    if not IsInToolsMode() then -- just to be 100% safe
-        return
-    end
-    if(lootQuality == nil) then
-        return
-    end
-
-    COverthrowGameMode:DropTempleItem(PlayerResource:GetSelectedHeroEntity(0), 100, 2, lootQuality, isArti)
-end
-
-function DebugTempleLevel(desiredLevel)
-    if not IsInToolsMode() then -- just to be 100% safe
-        return
-    end
-    local hero = PlayerResource:GetSelectedHeroEntity(0)
-    local xp = COverthrowGameMode.levelTable[desiredLevel]
-    hero:AddExperience(xp,false, true)
-    AddHeroExperience(hero, xp)
 end
