edcc35ac81f8e88b1fae4eca81fecde673a2bace
c <fdsatrewq3@gmail.com>
2024-05-05 12:17:55 +0300

    Better duct tape

--- game/scripts/vscripts/overrides/CDOTABaseAbility.lua
+++ game/scripts/vscripts/overrides/CDOTABaseAbility.lua
@@ -1,5 +1,6 @@
 -- Guard in case valve decide to rename CDOTABaseAbility to destroy every custom game (happened at least once already with GameRules)...
 if(CDOTABaseAbility == nil) then
+    print("Valve did something to break data driven. Fix manually...")
     return
 end
 
@@ -15,10 +16,6 @@ function CDOTABaseAbility:_ToggleAltCast(altCasted)
     self._isAbilityAltCasted = not self:IsAltCasted()
 end
 
-if(CDOTA_Ability_DataDriven ~= nil and CDOTA_Ability_DataDriven._internalApplyDataDrivenModifier == nil) then
-    CDOTA_Ability_DataDriven._internalApplyDataDrivenModifier = CDOTA_Ability_DataDriven.ApplyDataDrivenModifier
-end
-
 -- Duct tape to make lua compat with data driven abilities
 function CDOTABaseAbility:ApplyDataDrivenModifier(caster, target, modifierName, modifierTable)
     -- Should be safe and fast way to determine data driven ability
@@ -26,16 +23,12 @@ function CDOTABaseAbility:ApplyDataDrivenModifier(caster, target, modifierName, 
         -- Valve renamed or removed data driven, too bad...
         if(CDOTA_Ability_DataDriven == nil) then
             print("Valve did something to break data driven. Fix manually...")
+            return nil
         else
-            -- Calls data driven AddNewModifier
-            if(CDOTA_Ability_DataDriven._internalApplyDataDrivenModifier) then
-                CDOTA_Ability_DataDriven._internalApplyDataDrivenModifier(self, caster, target, modifierName, modifierTable)
-            else
-                -- Last resort to make all work
-                CDOTA_Ability_DataDriven.ApplyDataDrivenModifier(self, caster, target, modifierName, modifierTable)
-            end
+            -- Last resort to make all work
+            return CDOTA_Ability_DataDriven.ApplyDataDrivenModifier(self, caster, target, modifierName, modifierTable)
         end
     else
-		target:AddNewModifier(caster, self, modifierName, modifierTable)
+		return target:AddNewModifier(caster, self, modifierName, modifierTable)
     end
 end
